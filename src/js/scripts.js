5c82d25623a78214e9e9caf4602ce7cc775ea923
podluzhnyyK <podluzhnyy94@mail.ru>
2021-08-27 08:57:14 +0500

    commit init

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// sleder
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
